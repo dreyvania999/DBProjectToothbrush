4b12a570db2170a5f719c0393f6572722ffc65e3
ДрейИА <dreyvania999@gmail.com>
2022-09-14 15:05:38 +0300

    Начала работать кнопка, базу не выводит, ввод не начат

--- app/src/main/java/com/example/dbprojecttoothbrush/MainActivity.java
+++ app/src/main/java/com/example/dbprojecttoothbrush/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dbprojecttoothbrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,7 @@ import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button buttong;
+    Button AddToothbrush;
     Connection connection;
     String ConnectionResult = "";
     String NameOfTheToothbrush,TermOfUse_Day,Price,id;
@@ -26,15 +27,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TableLayout DBoutput = findViewById(R.id.DataTable);
-        UpdateDB(DBoutput);
-        buttong =findViewById(R.id.AddToothbrush);
+        AddToothbrush =findViewById(R.id.AddToothbrush);
+            GetTextFormSql(DBoutput);
+
 
     }
 
-        public void GetTextFormSql()
-    {
-
-
+        public void GetTextFormSql( TableLayout DBoutput)
+    {DBoutput.removeAllViews();
         try {
 
             DBHelper connectionHelper = new DBHelper();
@@ -42,19 +42,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             if (connection!=null)
             {
-                String query = "Select + From Masks";
+                String query = "Select * From Toothbrush";
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
 
 
                 while (resultSet.next())
                 {
-
-                    id=resultSet.getString(1);
-                    NameOfTheToothbrush= resultSet.getString(2);
-                    TermOfUse_Day=resultSet.getString(3);
-                    Price=resultSet.getString(4);
-
+                    UpdateDB(DBoutput,resultSet.getString(1),resultSet.getString(2),resultSet.getString(3),resultSet.getString(4));
                 }
             }
             else
@@ -71,11 +66,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public  void UpdateDB(TableLayout DBoutput){
-        int i=0;
-        GetTextFormSql();
-        DBoutput.removeAllViews();
-        do {
+    public  void UpdateDB(TableLayout DBoutput, String a,String b, String c,String d){
+
             TableRow DBoutputROW = new TableRow(this);
             DBoutputROW.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
             TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT);
@@ -83,43 +75,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             TextView outputID= new TextView(this);
             params.weight = 1.0f;
             outputID.setLayoutParams(params);
-            outputID.setText(id);
+            outputID.setText(a);
             DBoutputROW.addView(outputID);
 
             TextView NameOfTheToothbrushT= new TextView(this);
             params.weight = 2.0f;
             NameOfTheToothbrushT.setLayoutParams(params);
-            NameOfTheToothbrushT.setText(NameOfTheToothbrush);
+            NameOfTheToothbrushT.setText(b);
             DBoutputROW.addView(NameOfTheToothbrushT);
 
             TextView TermOfUse_DayT= new TextView(this);
             params.weight = 2.0f;
             TermOfUse_DayT.setLayoutParams(params);
-            TermOfUse_DayT.setText(TermOfUse_Day);
+            TermOfUse_DayT.setText(c);
             DBoutputROW.addView(TermOfUse_DayT);
 
             TextView Pricet= new TextView(this);
             params.weight = 2.0f;
             Pricet.setLayoutParams(params);
-            Pricet.setText(Price);
+            Pricet.setText(d);
             DBoutputROW.addView(Pricet);
 
             DBoutput.addView(DBoutputROW);
-            i+=1;
 
-        } while (i<=10);
+
     }
 
     @Override
     public void onClick(View view) {
-        TableLayout DBoutput = findViewById(R.id.DataTable);
-
-
 
         switch (view.getId()){
 
             case R.id.AddToothbrush:
-                UpdateDB(DBoutput);
+                Intent intent =new Intent(this,ToothbrushActivity.class);
+                startActivity(intent);
                 break;
 
         }
--- app/src/main/java/com/example/dbprojecttoothbrush/ToothbrushActivity.java
+++ app/src/main/java/com/example/dbprojecttoothbrush/ToothbrushActivity.java
@@ -2,13 +2,70 @@ package com.example.dbprojecttoothbrush;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ToothbrushActivity extends AppCompatActivity {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ToothbrushActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText TermOfUse_Day,Price,editNameToothbrush;
+    Button AddButton,BackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.toothbrush);
+        AddButton =findViewById(R.id.AddButton);
+        TermOfUse_Day =findViewById(R.id.TermOfUse_Day);
+        Price =findViewById(R.id.Price);
+        editNameToothbrush =findViewById(R.id.editNameToothbrush);
+        BackButton =findViewById(R.id.BackButton);
     }
-}
+    public void GetTextFormSql()
+    {
+        try {
+
+            DBHelper connectionHelper = new DBHelper();
+            Connection connection = connectionHelper.connectionClass();
+
+            if (connection!=null)
+            {
+                String query = "INSERT INTO Toothbrush()";
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+
+            }
+            else
+            {
+                //ConnectionResult="Check Connection";
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+            switch (view.getId()){
+
+                case R.id.BackButton:
+                    Intent intent =new Intent(this,MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+            }
+        }
+    }
