6419e77dfaa94a961fd82d0bd0cca8b119c5e81c
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-10-09 22:35:38 +0300

    Все работаетно проблемка с добавлением у меня последня запись добавилась в ередину, но никто не просил чтобы все было по айди, очистка полей мне тоже не нужна на странице добавления(при создании фейковой базы удобно)

--- app/src/main/java/com/example/dbprojecttoothbrush/MainActivity.java
+++ app/src/main/java/com/example/dbprojecttoothbrush/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.dbprojecttoothbrush;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,7 +13,9 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -19,9 +24,11 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Base64;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
@@ -45,6 +52,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         spinner.setOnItemSelectedListener(this);
         spinner2.setOnItemSelectedListener(this);
         GetTextFormSql(DBoutput);
+        DBHelper.id=-1;
+        SActivateSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TableLayout DBoutput = findViewById(R.id.DataTable);
+                GetTextFormSql(DBoutput);
+            }
+        });
 
         NameToothbrush.addTextChangedListener(new TextWatcher() {
             @Override
@@ -77,9 +91,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case 2 :
                 d= " ORDER BY Price";
                 break;
-            case 3 :
-                d= " ORDER BY NameOfTheToothbrush";
-                break;
         }
         return d;
     }
@@ -104,11 +115,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             DBHelper DBHelper = new DBHelper();
             connection = DBHelper.connectionClass();
-
+            addfirst( DBoutput);
             if (connection != null) {
                 String query = "Select * FROM Toothbrush";
                 query += " WHERE NameOfTheToothbrush LIKE '%" + NameToothbrush.getText().toString() + "%'";
-                if (SActivateSort.isChecked())
+                if (SActivateSort.isChecked()==true)
                 {
                     query += SelectedStrigOrder(spinner.getSelectedItemPosition())+" "+ SelectedASCDESCOrder(spinner2.getSelectedItemPosition());
                 }
@@ -116,7 +127,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Statement statement = connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
-                    UpdateDB(DBoutput, resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4));
+                    UpdateDB(DBoutput, resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4),resultSet.getString(5));
                 }
             } else {
                 Toast.makeText(this, "Проверьте подключение!", Toast.LENGTH_LONG).show();
@@ -126,24 +137,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void UpdateDB(TableLayout DBoutput, String ID, String TheToothbrush, String TermOfUse, String Price) {
+    public static String encodeImage(Bitmap bitmap) {
+        int prevW = 1000;
+        int prevH = bitmap.getHeight() * prevW / bitmap.getWidth();
+        int a = bitmap.getHeight();
+        int c = bitmap.getWidth();
+
+        Bitmap b = Bitmap.createScaledBitmap(bitmap, prevW, prevH, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return Base64.getEncoder().encodeToString(bytes);
+        }
+        return "";
+    }
+
+    private Bitmap getImgBitmap(String encodedImg) {
+        if (!encodedImg.equals("null")) {
+            byte[] bytes = new byte[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                bytes = Base64.getDecoder().decode(encodedImg);
+            }
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        }
+        return BitmapFactory.decodeResource(MainActivity.this.getResources(),
+                R.drawable.toothbrush);
+    }
+
+    public void UpdateDB(TableLayout DBoutput, String ID, String TheToothbrush, String TermOfUse, String Price,String img) {
 
         TableRow DBoutputROW = new TableRow(this);
         DBoutputROW.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(175, 175);
 
-        Button changeButton = new Button(this);
+        ImageView imageView = new ImageView(this);
 
         params.weight = 1.0f;
-        changeButton.setLayoutParams(params);
-        changeButton.setText(ID);
-        changeButton.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        changeButton.setId(Integer.parseInt(ID));
-        changeButton.setOnClickListener((view -> {
-            DBHelper.id = Integer.parseInt(ID);
-            startActivity(new Intent(MainActivity.this, ToothbrushActivity.class));
-        }));
-        DBoutputROW.addView(changeButton);
+        imageView.setLayoutParams(params);
+        imageView.setImageBitmap(getImgBitmap(img));
+        DBoutputROW.addView(imageView);
+
+
+        TextView Id = new TextView(this);
+        params.weight = 1.0f;
+        Id.setLayoutParams(params);
+        Id.setText(ID);
+        Id.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        DBoutputROW.addView(Id);
 
         TextView NameOfTheToothbrushT = new TextView(this);
         params.weight = 2.0f;
@@ -169,7 +211,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         deleteButton.setText("X");
         deleteButton.setId(Integer.parseInt(ID));
         DBoutputROW.addView(deleteButton);
+        DBoutputROW.setId(Integer.parseInt(ID));
+        DBoutputROW.setOnClickListener((view -> {
+            DBHelper.id = Integer.parseInt(ID);
+            startActivity(new Intent(MainActivity.this, ToothbrushActivity.class));
+        }));
         DBoutput.addView(DBoutputROW);
+
+
+    }
+    public void addfirst(TableLayout DBoutput) {
+
+        TableRow DBoutputROW = new TableRow(this);
+        DBoutputROW.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        TableRow.LayoutParams params = new TableRow.LayoutParams(175, 175);
+
+        TextView img = new TextView(this);
+        params.weight = 1.0f;
+        img.setLayoutParams(params);
+        img.setText("Фото");
+        img.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        DBoutputROW.addView(img);
+
+
+        TextView Id = new TextView(this);
+        params.weight = 1.0f;
+        Id.setLayoutParams(params);
+        Id.setText("ID");
+        Id.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        DBoutputROW.addView(Id);
+
+        TextView NameOfTheToothbrushT = new TextView(this);
+        params.weight = 2.0f;
+        NameOfTheToothbrushT.setLayoutParams(params);
+        NameOfTheToothbrushT.setText("Щетка");
+        DBoutputROW.addView(NameOfTheToothbrushT);
+
+        TextView TermOfUse_DayT = new TextView(this);
+        params.weight = 2.0f;
+        TermOfUse_DayT.setLayoutParams(params);
+        TermOfUse_DayT.setText("Срок службы");
+        DBoutputROW.addView(TermOfUse_DayT);
+
+        TextView Pricet = new TextView(this);
+        params.weight = 2.0f;
+        Pricet.setLayoutParams(params);
+        Pricet.setText("Цена");
+        DBoutputROW.addView(Pricet);
+
+        Button deleteButton = new Button(this);
+        deleteButton.setOnClickListener(this);
+
+        TextView del = new TextView(this);
+        params.weight = 2.0f;
+        del.setLayoutParams(params);
+        del.setText("Удалить");
+        DBoutputROW.addView(del);
+        DBoutput.addView(DBoutputROW);
+
 
     }
 
--- app/src/main/java/com/example/dbprojecttoothbrush/ToothbrushActivity.java
+++ app/src/main/java/com/example/dbprojecttoothbrush/ToothbrushActivity.java
@@ -1,26 +1,36 @@
 package com.example.dbprojecttoothbrush;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.Toast;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Base64;
 
 public class ToothbrushActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText TermOfUse_Day,Price,editNameToothbrush;
     Button AddButton,BackButton;
     Connection connection;
-
+    String Image;
     String ConnectionResult = "";
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,12 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
         Price =findViewById(R.id.Price);
         editNameToothbrush =findViewById(R.id.editNameToothbrush);
         BackButton =findViewById(R.id.BackButton);
+        imageView = findViewById(R.id.imageView);
+        imageView.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImg.launch(intent);
+        });
         if (DBHelper.id!=-1)
         {
             GetTextFormSql();
@@ -38,6 +54,22 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
         }
 
     }
+
+    public final ActivityResultLauncher<Intent> pickImg = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            if (result.getData() != null) {
+                Uri uri = result.getData().getData();
+                try {
+                    InputStream is = getContentResolver().openInputStream(uri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    imageView.setImageBitmap(bitmap);
+                    Image = MainActivity.encodeImage(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
     public void UpdateTextFormSql( ) {
         try {
             DBHelper dbHelper = new DBHelper();
@@ -45,8 +77,8 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
 
             if (connection != null) {
                 String query = "UPDATE Toothbrush SET NameOfTheToothbrush = '" + editNameToothbrush.getText().toString() +
-                        "', TermOfUse_Day =" + Integer.parseInt(TermOfUse_Day.getText().toString()) + ", Price = " + Double.parseDouble(Price.getText().toString()) +
-                        " WHERE Id = " + DBHelper.id;
+                        "', TermOfUse_Day =" + Integer.parseInt(TermOfUse_Day.getText().toString()) + ", Price = " + Double.parseDouble(Price.getText().toString()) + ", Image = '" +Image+
+                        "' WHERE Id = " + DBHelper.id;
                 Statement statement = connection.createStatement();
                 statement.executeUpdate(query);
 
@@ -65,7 +97,7 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
             connection = DBHelper.connectionClass();
 
             if (connection != null) {
-                String query = "INSERT INTO Toothbrush(NameOfTheToothbrush,TermOfUse_Day,Price) VALUES('"+(editNameToothbrush.getText().toString())+"',"+ Integer.parseInt(TermOfUse_Day.getText().toString())+","+Double.parseDouble(Price.getText().toString())+")";
+                String query = "INSERT INTO Toothbrush(NameOfTheToothbrush,TermOfUse_Day,Price,Image) VALUES('"+(editNameToothbrush.getText().toString())+"',"+ Integer.parseInt(TermOfUse_Day.getText().toString())+","+Double.parseDouble(Price.getText().toString())+", '"+ Image +"')";
                 Statement statement = connection.createStatement();
                 statement.executeUpdate(query);
 
@@ -77,6 +109,17 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
         } catch (Exception ex) {
             Toast.makeText(this, "Ошибка!", Toast.LENGTH_LONG).show();
         }
+    }
+    private Bitmap getImgBitmap(String encodedImg) {
+        if (!encodedImg.equals("null")) {
+            byte[] bytes = new byte[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                bytes = Base64.getDecoder().decode(encodedImg);
+            }
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        }
+        return BitmapFactory.decodeResource(ToothbrushActivity.this.getResources(),
+                R.drawable.toothbrush);
     }
     public void GetTextFormSql( ) {
         try {
@@ -92,6 +135,7 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
                     TermOfUse_Day.setText(resultSet.getString(3));
                     Price.setText(resultSet.getString(4));
                     editNameToothbrush.setText(resultSet.getString(2));
+                    imageView.setImageBitmap(getImgBitmap(resultSet.getString(5)));
                 }
             } else {
                 Toast.makeText(this, "Проверьте подключение!", Toast.LENGTH_LONG).show();
@@ -100,7 +144,6 @@ public class ToothbrushActivity extends AppCompatActivity implements View.OnClic
             Toast.makeText(this, "Ошибка!", Toast.LENGTH_LONG).show();
         }
     }
-
 
 
     @Override
